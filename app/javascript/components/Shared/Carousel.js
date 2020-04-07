6557001784bcb38aab80f6c0923f4b3b39d1331d
Jonny Marshall <jonnymarshall5@gmail.com>
2020-04-07 19:04:41 +0100

    changed text content

--- app/javascript/components/Shared/Carousel.js
+++ app/javascript/components/Shared/Carousel.js
@@ -19,30 +19,30 @@ export default class Carousel extends Component {
               {/* <%= image_tag 'slider/bg-1.jpg' %> */}
               <div className="carousel-caption text-left">
                 <h3 className="wow fadeInRight" data-wow-delay="0.2s">Adobe Premiere Pro Professional Training Series</h3>  
-                <h2 className="wow fadeInRight" data-wow-delay="0.4s">Editing >> Zero to Hero</h2>
-                <h4 className="wow fadeInRight" data-wow-delay="0.6s">Learn to Edit Like a Pro. Professional grade training from start to finish.</h4>
-                <a href="#" className="btn btn-lg btn-common btn-effect wow fadeInRight" data-wow-delay="0.9s">Download</a>
-                <a href="#" className="btn btn-lg btn-border wow fadeInRight" data-wow-delay="1.2s">Get Started!</a>
+                <h2 className="wow fadeInRight" data-wow-delay="0.4s">Editing: Zero to Hero</h2>
+                <h4 className="wow fadeInRight" data-wow-delay="0.6s">Learn to Edit Like a Pro.</h4>
+                <a href="#" className="btn btn-lg btn-common btn-effect wow fadeInRight" data-wow-delay="0.9s">Start Learning</a>
+                {/* <a href="#" className="btn btn-lg btn-border wow fadeInRight" data-wow-delay="1.2s">Get Started!</a> */}
               </div>
             </div>
             <div className="carousel-item">
             <img src="/assets/slider/bg-3-0d8b8f955e15df6b03c8c8c9c2a2eddf89ebcd07f821865cc2b7e292388f33c5.jpg" />
               {/* <%= image_tag 'slider/bg-3.jpg' %> */}
               <div className="carousel-caption text-center">
-                <h3 className="wow fadeInDown" data-wow-delay="0.3s">Bundled With Tons of</h3>
-                <h2 className="wow bounceIn" data-wow-delay="0.6s">Cutting-edge Features</h2> 
-                <h4 className="wow fadeInUp" data-wow-delay="0.9s">Parallax, Video, Product, Premium Addons and More...</h4>
-                <a href="#" className="btn btn-lg btn-common btn-effect wow fadeInUp" data-wow-delay="1.2s">View Works</a>
+                <h3 className="wow fadeInDown" data-wow-delay="0.3s">Taught by Industry Pros</h3>
+                <h2 className="wow bounceIn" data-wow-delay="0.6s">Industry-fit training</h2> 
+                <h4 className="wow fadeInUp" data-wow-delay="0.9s">Professional grade training from start to finish.</h4>
+                <a href="#" className="btn btn-lg btn-common btn-effect wow fadeInUp" data-wow-delay="1.2s">Start Learning</a>
               </div>
             </div>
             <div className="carousel-item">
               <img src="/assets/slider/bg-2-2b65da09eb9a13b9daea9eda0f95d5259e98be439ea6cd16f24fbf69fb3fadcf.jpg" />
               {/* <%= image_tag 'slider/bg-2.jpg' %> */}
               <div className="carousel-caption text-center">
-                <h3 className="wow fadeInDown" data-wow-delay="0.3s">Ready For</h3>
-                <h2 className="wow fadeInRight" data-wow-delay="0.6s">Multi-purpose Websites</h2> 
-                <h4 className="wow fadeInUp" data-wow-delay="0.6s">App, Business, SaaS and Landing Pages</h4>
-                <a href="#" className="btn btn-lg btn-border wow fadeInUp" data-wow-delay="0.9s">Purchase</a>
+                <h3 className="wow fadeInDown" data-wow-delay="0.3s">Edit it all</h3>
+                <h2 className="wow fadeInRight" data-wow-delay="0.6s">Learn to edit <i>anything.</i></h2> 
+                <h4 className="wow fadeInUp" data-wow-delay="0.6s">Learn to edit movies, documentaries, music videos, commercials and more.</h4>
+                <a href="#" className="btn btn-lg btn-border wow fadeInUp" data-wow-delay="0.9s">Start Learning</a>
               </div>
             </div>
           </div>
